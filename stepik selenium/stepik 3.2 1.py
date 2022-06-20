388d71a8575db4918694f25c912c8648ee1097ac
Valeriia Bakanova <valeriia.b@gowombat.team>
2022-06-20 14:15:59 +0300

    ассерт с указанием ошибки

--- stepik selenium/stepik 3.2 1.py
+++ stepik selenium/stepik 3.2 1.py
@@ -4,7 +4,7 @@ actual_result = 11
 
 def test_input_text(expected_result, actual_result):
     # ваша реализация, напишите assert и сообщение об ошибке
-    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}" # указание ошибки ф-строкой
 
 
 print(f"expected {expected_result}, got {actual_result}")
